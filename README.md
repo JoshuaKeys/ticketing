f81b953663dee1ea346032062903609679a0736d
JoshuaKeys <avwerosuoghene.oguma1@gmail.com>
2022-12-06 11:30:57 +0200

    fixed MR conflicts

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
+# Ticketing 
 Hello World
--- auth/src/db/connect-db.ts
+++ auth/src/db/connect-db.ts
@@ -1,6 +1,7 @@
 import { connect } from "mongoose";
 
 const connectDb = async () => {
+    
     try {
         const conn = await connect(process.env.MONGO_URI!)
         console.log('Connected to Auth Database')
